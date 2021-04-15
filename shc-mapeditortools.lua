3f146311f2b48ffe39331ebbb8bf7fe20109160a
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2021-04-15 21:44:13 +0200

    [DOC/MIRROR] Added mirroring order and requested @author statements

--- shc-mapeditortools.lua
+++ shc-mapeditortools.lua
@@ -4,8 +4,13 @@
 -- ///////////////
 
 
--- help text is created during start-up and can not be changed without restart (?)
--- TODO: change help text according to new modification system
+--[[
+  Help text displayed by the console.
+  
+  Help text is created during start-up and can not be changed without restart?
+
+  @TheRedDaemon
+]]--
 HELP = [[
 This console is used to configure the additional map editor features.
 
@@ -29,6 +34,7 @@ The following features are implemented and currently applied in the order they a
     Uses the coordinates of two clicks (the first one does nothing) to create a shape.
     WARNING: Currently only 200 actions are supported. Big shapes, especially when mirrored, reach
              this limit very fast. So do not be surprised if only one half of a shape appears.
+    WARNING: The first coordinate is only invalidated after use or after disabling the shape brush.
              
     BRUSH_SHAPE -> deactivate/activate
         boolean             false or true
@@ -66,6 +72,10 @@ The following features are implemented and currently applied in the order they a
         "diagonal_x"
         "diagonal_y"
         "point"             mirror around the center of the map
+        
+    MIRROR_ORDER -> order of coordinates after mirroring
+        "shape"             draws original shape first, then first mirror, then second mirror
+        "coord"             every single coordinate of the original shape is mirrored one after another
 
 Available commands:
         help                display this help text again
@@ -84,8 +94,9 @@ Available commands:
 
 
 -- mirror config
-MIRROR_MODE = "off"
-MIRROR_MODE2 = "off"
+MIRROR_MODE = "off" -- mirroring type: "off", "horizontal", "vertical", "diagonal_x", "diagonal_y", "point"
+MIRROR_MODE2 = "off" -- second mirroring type: same values as first
+MIRROR_ORDER = "coord" -- order of coordinates after mirroring: "shape", "coord"
 
 
 -- brush config
@@ -100,7 +111,7 @@ BRUSH_SPRAY_INT = 0.25 -- intensity -> 0 to 1, if random number bigger, skips th
 
 
 -- shape config
-BRUSH_SHAPE_SHAPE = "line" -- shapes: "line", "rect", "rect45", "circle" -- @TheRedDaemon: Not happy about the name. Suggestions?
+BRUSH_SHAPE_SHAPE = "line" -- shapes: "line", "rect", "rect45", "circle" -- @TheRedDaemon: Not happy about the value name. Suggestions?
 -- shape data
 BRUSH_SHAPE_LAST = {} -- last selected points
 
@@ -110,7 +121,13 @@ BRUSH_SHAPE_LAST = {} -- last selected points
 -- //////////////////////
 
 
--- source: https://stackoverflow.com/a/1252776
+--[[
+  Returns "true" should the table be empty.
+
+  source: https://stackoverflow.com/a/1252776
+  
+  @TheRedDaemon
+]]--
 function isTableEmpty(t)
   local next = next
   return next(t) == nil
@@ -118,11 +135,14 @@ end
 
 
 --[[
+  Gets the number of entries in a table.
+  
   Apparently there is #, but this only works on array-style tables.
-   -> Here good enough, but these functions should work in general.
   O(N) is the most efficient this can get without meta structures (a "count" variable for example)
+  source: https://stackoverflow.com/a/2705804
+  
+  @TheRedDaemon
 ]]--
--- source: https://stackoverflow.com/a/2705804
 function getTableLength(t)
   local count = 0
   for _ in pairs(t) do count = count + 1 end
@@ -130,15 +150,26 @@ function getTableLength(t)
 end
 
 
--- source: https://scriptinghelpers.org/questions/4850/how-do-i-round-numbers-in-lua-answered
--- Note: 0.5 -> 1, -0.5 -> 0
+--[[
+  A simple rounding to full numbers function.
+  
+  Note: 0.5 -> 1, but -0.5 -> 0
+  source: https://scriptinghelpers.org/questions/4850/how-do-i-round-numbers-in-lua-answered
+  
+  @TheRedDaemon
+]]--
 function round(x)
   return x + 0.5 - (x + 0.5) % 1
 end
 
 
--- find coord duplicates in "coordTable"
--- if "display" true -> prints duplicates
+--[[
+  Find coord duplicates in "coordTable".
+  
+  If "display" true -> prints duplicates
+  
+  @TheRedDaemon
+]]--
 function countCoordDuplicates(coordTable, display)
   if display ~= false and display ~= true then
     display = false -- default
@@ -170,7 +201,13 @@ end
 -- // spray brush //
 
 
--- generates a random deviation for the spray brush using the size
+--[[
+  Generates a random deviation for the spray brush.
+  
+  Max deviation for both axes is set by "size".
+  
+  @TheRedDaemon
+]]--
 function randomSprayDeviation(size)
   local rand = round((math.random()^BRUSH_SPRAY_EXP) * size)
   rand = math.random() < 0.5 and -rand or rand -- plus or minus
@@ -181,9 +218,14 @@ end
 -- // shape brush //
 
 
--- Fills coordTable with {x,y} int coordinates using Bresenham's line algorithm.
--- According to source, this version does not guarantee a coordinate order. Could be x0, y0 to x1, y1 or vise versa.
--- source: https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm 
+--[[
+  Fills coordTable with {x,y} int coordinates using Bresenham's line algorithm.
+  
+  According to source, this version does not guarantee a coordinate order. Could be x0, y0 to x1, y1 or vise versa.
+  source: https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+
+  @TheRedDaemon
+]]--
 function fillWithLineCoords(x0, y0, x1, y1, coordTable)      
   local dx = math.abs(x1 - x0)
   local sx = x0 < x1 and 1 or -1
@@ -206,8 +248,14 @@ function fillWithLineCoords(x0, y0, x1, y1, coordTable)
 end
 
 
--- create coords in such a way that the player sees a rectangle
--- used weird own function, maybe change to four "fillWithLineCoords" one day
+--[[
+  Create coords in such a way that the player sees a rectangle.
+  
+  Used own function, maybe change to four "fillWithLineCoords" one day?
+  (But this would only save visual space in this file.)
+
+  @TheRedDaemon
+]]--
 function fillWithRectCoords(x0, y0, x1, y1, coordTable)
   local xDiff = x1 - x0
   local yDiff = y1 - y0
@@ -241,7 +289,11 @@ function fillWithRectCoords(x0, y0, x1, y1, coordTable)
 end
 
 
--- create coords in such a way that the player sees a rectangle rotated by 45 degree
+--[[
+  Create coords in such a way that the player sees a rectangle rotated by 45 degree.
+
+  @TheRedDaemon
+]]--
 function fillWithRect45Coords(x0, y0, x1, y1, coordTable)
   -- prevent duplicates (could also use line algorithm instead?)
   if x0 == x1 or y0 == y1 then
@@ -272,9 +324,13 @@ end
 
 
 --[[
--- as long as there is a limit on the input actions, this function does not make much sense
+-- @TheRedDaemon: as long as there is a limit on the input actions, this function does not make much sense
 
--- create coords in such a way that the player sees a filled rectangle rotated by 45 degree
+
+  Create coords in such a way that the player sees a filled rectangle rotated by 45 degree.
+
+  @TheRedDaemon
+
 function fillWithFilledRect45Coords(x0, y0, x1, y1, coordTable)  
   for i = x0, x1, x0 < x1 and 1 or -1 do
     for j = y0, y1, y0 < y1 and 1 or -1 do
@@ -286,12 +342,15 @@ end
 
 
 --[[
-  function used to draw a circle using Mid-Point Circle Drawing Algorithm
+  Function used to draw a circle using Mid-Point Circle Drawing Algorithm.
+  
   source: https://www.geeksforgeeks.org/mid-point-circle-drawing-algorithm/
   
-  Note (from TheRedDaemon):
+  Note:
     - originally used Bresenham’s Algorithm (source: https://www.geeksforgeeks.org/bresenhams-circle-drawing-algorithm/)
     - but it produced duplicates I was unable to remove
+    
+  @TheRedDaemon
 ]]--
 function fillWithCircleCoords(x_centre, y_centre, xr, yr, coordTable)
   local r = round(math.sqrt((x_centre - xr)^2 + (y_centre - yr)^2))
@@ -342,15 +401,17 @@ end
 -- // mirror support //
 
 
+-- @gynt
 function applyMirror(x, y, size, mirrorMode)
   local newx = x
   local newy = y
   
   --[[
-    SUGGESTION: "point" is more a rotation
+    @TheRedDaemon:
+  
+    SUGGESTION: "point" is also a rotation
     -> maybe add the ability to apply a point rotation to the action instead
       - so that I could mirror the action in 4 corners, or even 8
-      - point mirror 1 + point mirror 2 does what it should (looks like only one mirror), but maybe not what is expected
   ]]--
   if mirrorMode == "point" then
     newx = (399 - x) - (size - 1)
@@ -380,12 +441,18 @@ end
 -- ///////////////////////
 
 
--- @TheRedDaemon
--- Uses a previous click and the current click to generate NEW coordlist that forms a shape.
--- Will return an empty coordlist after it stored the first coordinate for the shape.
--- Shapes alone should not produce duplicates.
--- WARNING: Currently only the first coordinate (coordlist[1]) will be used.
--- WARNING: Currently maximal 200 coords can be applied. This easily results in incomplete big shapes.
+--[[
+  Uses a previous click and the current click to generate NEW coordlist that forms a shape.
+  
+  Will return an empty coordlist after it stored the first coordinate for the shape.
+  Shapes alone should not produce duplicates.
+  
+  WARNING: Currently only the first coordinate (coordlist[1]) will be used.
+  WARNING: Currently maximal 200 coords can be applied. This easily results in incomplete big shapes.
+  WARNING: The first coordinate is only invalidated after use or after disabling the shape brush.
+  
+  @TheRedDaemon
+]]--
 function applyShape(coordlist, size)
   if not BRUSH_SHAPE or isTableEmpty(coordlist) then
     BRUSH_SHAPE_LAST = {} -- remove last entry if brush inactive (sadly happens on every call)
@@ -422,10 +489,14 @@ function applyShape(coordlist, size)
 end
 
 
--- @TheRedDaemon
--- Modifies all coords in coordlist by adding a random deviation.
--- If BRUSH_SPRAY_INT < 1, this function will build a new coordlist with the remaining coords. 
--- Produces duplicates.
+--[[
+  Modifies all coords in coordlist by adding a random deviation.
+  
+  If BRUSH_SPRAY_INT < 1, this function will build a new coordlist with the remaining coords. 
+  Produces coordinate duplicates.
+
+  @TheRedDaemon
+]]--
 function applySpray(coordlist, size)
   if not BRUSH_SPRAY then
     return coordlist
@@ -457,23 +528,66 @@ function applySpray(coordlist, size)
 end
 
 
--- mirror all coordinates and add the new coordinates to the coordlist
--- Produces duplicates.
+--[[
+  Mirror all coordinates and return a modified or new "coordlist" with all coordinates.
+  
+  The coordinate structure after mirroring might have some effects on the results.
+  For example, bigger rocks might block each other.
+  The order types are shown here, using an example where the shape "line" is used to draw a 3 coord line,
+  which is then mirrored two times (first with vertical mirror, then horizontal):
+  
+   1     |     4                 1     |     4                 1     |     2
+    2    |    7                   2    |    5                   5    |    6  
+     3   |  10                     3   |   6                     9   |  10   
+  _______|_______               _______|_______               _______|_______
+         |                             |                             |       
+     11  |  12                     9   |  12                     11  |  12   
+    8    |    9                   8    |    11                  7    |    8  
+   5     |     6                 7     |     10                3     |     4 
+   
+  original order                ordered by original shape     ordered by original coordinates
+  (not supported anymore)       MIRROR_ORDER: "shape"         MIRROR_ORDER: "coord"
+  
+  Produces coordinate duplicates.
+  
+  @gynt (original), @TheRedDaemon
+]]--
 function applyMirrors(coordlist, size)
-  local dummyCoordTable = {}
-  for _, coord in pairs(coordlist) do
-    if MIRROR_MODE ~= "off" then
-      local firstMirror = applyMirror(coord[1], coord[2], size, MIRROR_MODE)
-      table.insert(dummyCoordTable, firstMirror)
-      
-      if MIRROR_MODE2 ~= "off" then
-        table.insert(dummyCoordTable, applyMirror(coord[1], coord[2], size, MIRROR_MODE2))
-        table.insert(dummyCoordTable, applyMirror(firstMirror[1], firstMirror[2], size, MIRROR_MODE2))
+  
+  if MIRROR_ORDER == "coord" then
+    local newCoordTable = {}
+    
+    for _, coord in ipairs(coordlist) do
+      table.insert(newCoordTable, coord)
+    
+      if MIRROR_MODE ~= "off" then
+        local firstMirror = applyMirror(coord[1], coord[2], size, MIRROR_MODE)
+        table.insert(newCoordTable, firstMirror)
+        
+        if MIRROR_MODE2 ~= "off" then
+          table.insert(newCoordTable, applyMirror(coord[1], coord[2], size, MIRROR_MODE2))
+          table.insert(newCoordTable, applyMirror(firstMirror[1], firstMirror[2], size, MIRROR_MODE2))
+        end
       end
     end
-  end
-  for _, coord in pairs(dummyCoordTable) do
-    table.insert(coordlist, coord)
+    coordlist = newCoordTable
+    
+  elseif MIRROR_ORDER == "shape" then
+  
+    if MIRROR_MODE ~= "off" then
+      local numberOfCoords = #coordlist -- @TheRedDaemon: Should only have number indexes, so this should be fine.
+      for index = 1, numberOfCoords do
+        table.insert(coordlist, applyMirror(coordlist[index][1], coordlist[index][2], size, MIRROR_MODE))
+      end
+      
+      if MIRROR_MODE2 ~= "off" then
+        for index = 1, numberOfCoords * 2 do
+          table.insert(coordlist, applyMirror(coordlist[index][1], coordlist[index][2], size, MIRROR_MODE2))
+        end
+      end
+    end
+  else
+    print("No valid mirror order: " ..MIRROR_ORDER)
   end
   
   -- print("Number of coord duplicates after applying mirrors: " ..countCoordDuplicates(coordlist)) -- debug
@@ -482,50 +596,72 @@ function applyMirrors(coordlist, size)
 end
 
 
+-- @gynt
 function erase(x, y, brush)
   return applyBrush(x, y, brush)
 end
 
+
+-- @gynt
 function setTerrainTypeEarth(x, y, brush)
   return applyBrush(x, y, brush)
 end
 
- -- change is actually a signed byte, it can be -1 for example to decrease terrain height
+
+--[[
+  change is actually a signed byte, it can be -1 for example to decrease terrain height
+
+  @gynt
+]]--
 function changeTerrainHeight(x, y, brush, change)
   return transformTerrain(x, y, brush, change)
 end
 
+
+-- @gynt
 function levelTerrain(x, y, brush, unknown)
   return transformTerrain(x, y, brush, unknown)
 end
 
+
+-- @gynt
 function minHeightTerrain(x, y, brush, unknown)
   return transformTerrain(x, y, brush, unknown)
 end
 
+
+-- @gynt
 function createPlateau(x, y, brush, intensity)
   return transformTerrain(x, y, brush, intensity)
 end
 
+
+-- @gynt
 function createHill(x, y, intensity)
   return applyBrush(x, y, intensity)
 end
 
+
+-- @gynt
 function placeAnimal(x, y, animalType)
   return applyBrush(x, y, animalType)
 end
 
 
- -- functions that now do the actual thing required :)
- 
--- @TheRedDaemon
--- general function to create coordinatelist
+-- @gynt: functions that now do the actual thing required :)
+
+
+--[[
+  General function to create coordinatelist.
+  
+  @TheRedDaemon 
+]]--
 function applyCoordModification(x, y, size)
   -- print("Current Coord: " ..x ..":" ..y) -- debug
   
   local coordinatelist = {{x, y}}
   for _, func in ipairs(ACTIVE_TRANSFORMATIONS) do -- ipairs to guarantee order
-    coordinatelist = func(coordinatelist, 1)
+    coordinatelist = func(coordinatelist, size)
   end
   
   -- print("Total number of coords: " ..getTableLength(coordinatelist)) -- debug
@@ -533,6 +669,8 @@ function applyCoordModification(x, y, size)
   return coordinatelist
 end
 
+
+-- @gynt
 function applyBrush (x, y, brush)
   coordinatelist = applyCoordModification(x, y, 1)  
   
@@ -543,6 +681,8 @@ function applyBrush (x, y, brush)
   return coordinatelist
 end
 
+
+-- @gynt
 function transformTerrain(x, y, brush, change)
   coordinatelist = applyCoordModification(x, y, 1)  
   
@@ -554,6 +694,8 @@ function transformTerrain(x, y, brush, change)
   return coordinatelist
 end
 
+
+-- @gynt
 function setTerrainType(x, y, brush, terrainType, unknown)
   coordinatelist = applyCoordModification(x, y, 1)  
   
@@ -566,7 +708,12 @@ function setTerrainType(x, y, brush, terrainType, unknown)
   return coordinatelist
 end
 
- -- objectType == 20 means the object is a rock and math.floor(rockType / 4)+1 will indicate the rock size
+
+--[[
+  objectType == 20 means the object is a rock and math.floor(rockType / 4)+1 will indicate the rock size
+  
+  @gynt
+]]--
 function placeTreeOrRock(x, y, objectType, rockType)
   
   if objectType == 20 then -- adjust with a translation for the fact that a rock can be larger than 1x1
@@ -590,9 +737,14 @@ end
 -- ///////////////////////
 
 
--- @TheRedDaemon
--- Coordinate modification order
--- Lua was not seemingly not able to add global function refs at the start (before definition?)
+--[[
+  Coordinate modification order
+  
+  Lua was not seemingly not able to add global function refs at the start (before definition?).
+  So the array is created here.
+
+  @TheRedDaemon
+]]--
 ACTIVE_TRANSFORMATIONS = {
    applyShape,              -- 1. draw shape
    applySpray,              -- 2. mess it up
